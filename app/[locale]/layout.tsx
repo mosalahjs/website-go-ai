994c70cd3f795998dda2f3ffb1c6c60684fefb5a
mosalahjs <mosalahgoai6@gmail.com>
2025-09-28 17:35:30 +0300

    enhance navbar

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
-import Navbar from "@/components/navbar";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getLocale } from "next-intl/server";
 import { getDirection } from "@/i18n/i18n-confige";
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,5 @@
 import Container from "@/components/shared/Container";
 import { getTranslations, getLocale } from "next-intl/server";
-import { RevealSection } from "@/components/shared/RevealSection";
-import { Card, CardContent } from "@/components/ui/card";
 import Landing from "@/components/landing";
 
 export default async function Home() {
